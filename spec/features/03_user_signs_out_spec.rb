2b3396a99fe6e98577be903f56927e62aa6c19d5
Michael Silberstein <mikesilb123@gmail.com>
2017-04-29 16:22:44 -0400

    Adds sign out functionality

---
+++ spec/features/03_user_signs_out_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "user signs out" do
+  let!(:user)   { FactoryGirl.create(:user) }
+
+  scenario "a logged in user successfully logs out" do
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Log In"
+    click_link "Logout"
+
+    expect(page).to have_content("Signed out successfully.")
+    expect(page).to_not have_content("Logged in as")
+  end
+end

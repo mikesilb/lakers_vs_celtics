d160554f68a873c043ff04b7df3703f6b4c0580b
Michael Silberstein <mikesilb123@gmail.com>
2017-05-01 17:09:29 -0400

    Reset database prior to Heroku deployment

--- db/migrate/20170501163902_rename_nbafinals.rb
+++
@@ -1,5 +0,0 @@
-class RenameNbafinals < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :nba_finals, :nbafinals
-  end
-end
--- db/migrate/20170501180748_rename_team_names_again.rb
+++
@@ -1,5 +0,0 @@
-class RenameTeamNamesAgain < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :team_names, :teamnames
-  end
-end

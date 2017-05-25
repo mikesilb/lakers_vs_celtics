4fa0c56c1be95752e57ddd9d37255fa1573f8b16
Michael Silberstein <mikesilb123@gmail.com>
2017-05-24 20:49:11 -0400

    Add more passing post review tests

--- spec/features/09_user_edits_reviews_spec.rb
+++ spec/features/09_user_edits_reviews_spec.rb
@@ -35,38 +35,38 @@ feature "visitors can edit a review" do
     expect(page).to_not have_content "Edit Review"
   end
 
-#   scenario "and should be shown error message if updated review has no rating" do
-#     login_as(user)
-#     visit root_path
-#
-#     first(:link, nbafinal.year).click
-#
-#     click_link "Edit Review"
-#
-#     select("", from: "Rating")
-#     fill_in "Review", with: "I have a new found respect for this colour"
-#     click_button "Submit Review"
-#
-#     save_and_open_page
-#     expect(page).to have_content "Rating must be between 1 - 5"
-#     expect(page).to have_content "Edit Review"
-#     expect(page).to_not have_content "Your review is successfully saved!"
-#   end
-#
-#   scenario "and should be shown error message if updated review has no body" do
-#     login_as(user)
-#     visit root_path
-#
-#     first(:link, nbafinal.year).click
-#
-#     click_link "Edit Review"
-#
-#     select(4, from: "Rating")
-#     fill_in "Review", with: ""
-#     click_button "Submit Review"
-#
-#     expect(page).to have_content "Body can't be blank"
-#     expect(page).to have_content "Edit Review"
-#     expect(page).to_not have_content "Your review is successfully saved!"
-#   end
+  scenario "and should be shown error message if updated review has no rating" do
+    login_as(user)
+    visit root_path
+
+    first(:link, nbafinal.year).click
+
+    click_link "Edit Review"
+
+    select("", from: "Rating")
+    fill_in "Review", with: "I have a new found respect for this NBA Final"
+    click_button "Submit Review"
+
+    expect(page).to have_content "Rating can't be blank"
+    expect(page).to have_content "Rating is not a number"
+    expect(page).to have_content "Edit Review"
+    expect(page).to_not have_content "Your review is successfully saved!"
+  end
+
+  scenario "and should be shown error message if updated review has no body" do
+    login_as(user)
+    visit root_path
+
+    first(:link, nbafinal.year).click
+
+    click_link "Edit Review"
+
+    select(4, from: "Rating")
+    fill_in "Review", with: ""
+    click_button "Submit Review"
+
+    expect(page).to have_content "Body can't be blank"
+    expect(page).to have_content "Edit Review"
+    expect(page).to_not have_content "Your review is successfully saved!"
+  end
 end

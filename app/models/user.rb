062ade0f730420bae620f92a8c684d08a12ad056
Michael Silberstein <mikesilb123@gmail.com>
2017-05-30 22:15:03 -0400

    Hound fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,4 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
 end
--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,5 +1,4 @@
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
